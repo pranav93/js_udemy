36d006db8c84b696453004af1e7daea13431dfd2
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-03-21 09:06:34 +0530

    2-JS-basics WIP

---
+++ 2-JS-basics/06/script.js
@@ -0,0 +1,17 @@
+// IF/ELSE STATEMENTS
+
+var firstName = 'John';
+var civilStatus = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married.')
+} else {
+    console.log('Hopefully ' + firstName + ' will marry soon.')
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married.')
+} else {
+    console.log('Hopefully ' + firstName + ' will marry soon.')
+}
