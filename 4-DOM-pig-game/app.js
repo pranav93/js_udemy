976820484cc35931752ba74b71dd7be73d18fa42
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-03-22 21:29:10 +0530

    4-DOM-pig-game WIP

--- 4-DOM-pig-game/app.js
+++ 4-DOM-pig-game/app.js
@@ -8,3 +8,37 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScores, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    var dice, diceDOM;
+    
+    // 1. Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+    console.log(dice);
+
+    // 2. Display the result
+    diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+
+});
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
