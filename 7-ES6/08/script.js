3089fa13b27f2a31a70b8fda59d3a8e88e238588
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-03-31 13:44:06 +0530

    7-ES6 WIP

---
+++ 7-ES6/08/script.js
@@ -0,0 +1,31 @@
+// THE    SPREAD    OPERATOR
+
+function addFourAges(a, b, c, d) {
+    return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1);
+
+// ES5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2);
+
+// ES6
+const sum3 = addFourAges(...ages);
+console.log(sum3);
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+
+const bigFamily = [...familySmith, 'Lily', ...familyMiller];
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+let all = [h, ...boxes];
+all = Array.from(all);
+for (let el of all) {
+    el.style.color = 'purple';
+}
