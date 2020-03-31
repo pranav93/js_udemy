c10c1dd4ad651040102e3c2b6949f4749448039d
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-03-31 11:07:00 +0530

    7-ES6 WIP

---
+++ 7-ES6/04/script.js
@@ -0,0 +1,23 @@
+// LECTURE    :    ARROW FUNCTIONS    ->    BASICS
+
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function (el) {
+    return 2020 - el;
+});
+console.log(ages5);
+
+// ES6
+let ages6 = years.map(el => 2020 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`;
+});
+console.log(ages6);
