a87d119f2a25ef0136b0105ab3becc1ead1ad83e
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-04-08 21:10:22 +0530

    added search view

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,7 +1,9 @@
 import Search from './models/Search';
 import * as searchView from "./views/searchView";
 import {
-    elements
+    elements,
+    renderLoader,
+    clearLoader
 } from "./views/base";
 
 /*Global state of the app
@@ -23,11 +25,13 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput();
         searchView.clearResults();
+        renderLoader(elements.searchRes);
 
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
