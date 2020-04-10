e1b24d48d328a2d57bc312f09ee3afda64c82b4d
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-04-10 16:42:17 +0530

    completed recipe servings update

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -97,3 +97,19 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateRecipeServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateRecipeServingsIngredients(state.recipe);
+    }
+    console.log(state.recipe);
+});
