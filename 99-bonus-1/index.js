4bcafebdd812686d661e75cc32ab3f3d7420ca44
Pranav Ambhore <ambhore.pranav@gmail.com>
2020-04-11 13:42:58 +0530

    99-bonus WIP

---
+++ 99-bonus-1/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf8');
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
+
+    if (pathName === '/products' || pathName === '/' ) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('This is the PRODUCTS page');            
+    } else if (pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(`This is the LAPTOP page ${id}`);
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('URL not found');
+    }
+});
+
+server.listen(1337, '127.0.0.1', () => {
+    console.log('Listening for requests now.');
+});
